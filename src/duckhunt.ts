6fedfef791d5b3c96502e0b56a55aab34a4bbd52
Tom Hiller <thrilleratplay@gmail.com>
2026-05-17 22:58:48 -0400

    fix: add duckhunt to globalthis

--- src/duckhunt.ts
+++ src/duckhunt.ts
@@ -347,7 +347,4 @@ const duckhunt = (callback?: () => void) => {
 
 export default duckhunt;
 
-// eslint-disable-next-line max-len
-// eslint-disable-next-line no-global-assign, sonarjs/no-globals-shadowing, @typescript-eslint/ban-ts-comment
-// @ts-ignore
 globalThis.duckhunt = duckhunt;
